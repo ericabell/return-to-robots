48fa40bceb215c65f15fe1b41059345152ec1f8e
Eric Abell <ericabell@users.noreply.github.com>
2017-08-22 12:26:52 -0500

    Basic project requirements complete

--- routes/users.js
+++ routes/users.js
@@ -7,47 +7,75 @@ const assert = require('assert');
 // collection name is robots
 let url = 'mongodb://localhost:27017/test';
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+let findUsers = function(callback) {
+  // Use connect method to connect to the server
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
 
-  findUsers(db, (docs) => {
-    // assign docs to data, just like we had it before
-    data.users = docs;
-    console.log(data);
-    db.close();
+    let collection = db.collection('robots');
+    collection.find({}).toArray( (err, docs) => {
+      assert.equal(err, null);
+      console.log(`Found user records`);
+      callback(docs);
+    });
   });
-
-});
-
-let findUsers = function(db, callback) {
-  let collection = db.collection('robots');
-  collection.find({}).toArray( (err, docs) => {
-    assert.equal(err, null);
-    console.log(`Found user records`);
-    callback(docs);
-  })
 }
+
+let findEmployedUsers = function(callback) {
+  // Use connect method to connect to the server
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    let collection = db.collection('robots');
+    collection.find({job: {$ne: null}}).toArray( (err, docs) => {
+      assert.equal(err, null);
+      console.log(`Found user records`);
+      callback(docs);
+    });
+  });
+}
+
+let findUnEmployedUsers = function(callback) {
+  // Use connect method to connect to the server
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    let collection = db.collection('robots');
+    collection.find({job: {$eq: null}}).toArray( (err, docs) => {
+      assert.equal(err, null);
+      console.log(`Found user records`);
+      callback(docs);
+    });
+  });
+}
+
 
 let router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('directory', data);
+  findUsers( (docs) => {
+    data.users = docs;
+    res.render('directory', data);
+  });
 });
 
 router.get('/employed', (req, res) => {
-  // get only the employed folks
-  let employedUsers = data.users.filter( (user) => {
-    console.log(user);
-    if( user.job === null ) {
-      // they are unemployed
-      return false;
-    }
-    return true;
-  });
-  res.render('directory', {users: employedUsers});
-})
+  findEmployedUsers( (docs) => {
+    data.users = docs;
+    res.render('directory', data);
+  })
+});
+
+router.get('/unemployed', (req, res) => {
+  findUnEmployedUsers( (docs) => {
+    data.users = docs;
+    res.render('directory', data);
+  })
+});
+
 
 
 module.exports = router;
