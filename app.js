636a9946f4decadb98db1c9e6664f425c9da1ffa
Eric Abell <ericabell@users.noreply.github.com>
2017-08-22 23:21:47 -0500

    Add morgan for logging

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+const morgan = require('morgan');
 
 let users = require('./routes/users.js');
 let app = express();
 
 app.use(express.static('public'));
+app.use(morgan('tiny'))
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
